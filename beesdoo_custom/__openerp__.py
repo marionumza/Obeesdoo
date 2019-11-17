fd75d2dcb773262ce12db7ddb9d75180692583a3
Augustin Borsu <a.borsu@gmail.com>
2019-11-17 15:01:17 +0100

    [REFACTOR] black

--- beesdoo_custom/__openerp__.py
+++ beesdoo_custom/__openerp__.py
@@ -1,29 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "beesdoo_custom",
-
-    'description': """
+    "name": "beesdoo_custom",
+    "description": """
         View and field definition specific to BEES' needs.
      """,
-
-    'author': "Beescoop - Cellule IT",
-    'website': "https://github.com/beescoop/Obeesdoo",
-
+    "author": "Beescoop - Cellule IT",
+    "website": "https://github.com/beescoop/Obeesdoo",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Sales Management',
-    'version': '12.0.0.1',
-
+    "category": "Sales Management",
+    "version": "12.0.0.1",
     # any module necessary for this one to work correctly
-    'depends': [
-        'beesdoo_product',
-        'purchase',
-        'easy_my_coop', # for product views
-    ],
-
+    "depends": ["beesdoo_product", "purchase", "easy_my_coop",],  # for product views
     # always loaded
-    'data': [
-        'views/beesdoo_product.xml',
-    ],
+    "data": ["views/beesdoo_product.xml",],
 }
