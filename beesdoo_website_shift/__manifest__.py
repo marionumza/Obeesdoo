3b7f75345b9d2a9f88c9dcf100263736565a2959
Rémy Taymans <remytaymans@gmail.com>
2020-01-26 10:53:48 +0100

    [MIG] beesdoo_website_shift: Migrate to 12.0

---
+++ beesdoo_website_shift/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017-2018 Rémy Taymans <remytaymans@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "BEES coop Website Shift",
+    "summary": """
+        Show available shifts for regular and irregular workers on the
+        website and let workers manage their shifts with an
+        easy web interface.
+    """,
+    "description": """
+    """,
+    "author": "Rémy Taymans",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Cooperative management",
+    "depends": ["website", "beesdoo_shift"],
+    "data": [
+        "views/shift_website_templates.xml",
+        "views/my_shift_website_templates.xml",
+        "views/res_config_views.xml",
+    ],
+}
